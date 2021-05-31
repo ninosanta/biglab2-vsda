b61903be641bd7c528379c4749261ebcb4ca033a
ANTONIO VESPA s284101 <44071720+mrwasp98@users.noreply.github.com>
2021-05-31 14:56:59 +0200

    user

---
+++ server/user_dao.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const sqlite = require("sqlite3");
+
+const db = new sqlite.Database('tasks.db', (err) => { if (err) throw err; });
+const bcrypt = require("bcrypt");
+
+/* DAO operations for validating users */
+
+exports.getUser = (email, password) => {
+    // username = email
+    return new Promise(
+        (resolve, reject) => {
+            const query = "SELECT * FROM user WHERE email = ?";
+            db.get(query, [email], (err, row) => {
+                if(err)
+                    reject(err);  // DB error
+                else if (row === undefined)
+                    resolve(false);  // user not found
+                else {
+                    /* user found -> password check! */
+                    bcrypt.compare(password, row.password).then(
+                        result => {
+                            if(result)  // password matched!
+                                resolve({id: row.id, username: row.email, name: row.name});
+                            else
+                                resolve(false);  // password did not match!
+                    )
+                }
+                    
+            })
+        }
+    );
+}
