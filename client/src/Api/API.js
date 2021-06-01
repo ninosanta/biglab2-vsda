579947345dd6a68ce42549811e9f14117e7dfe0c
ANTONIO VESPA s284101 <44071720+mrwasp98@users.noreply.github.com>
2021-06-01 10:24:57 +0200

    third

--- client/src/Api/API.js
+++ client/src/Api/API.js
@@ -78,7 +78,59 @@ async function updateTask(task) {
     });
 }
 
-const API = {deleteTask, addTask, getTasks, updateTask};
+async function userLogin(username, password) {
+    return new Promise((resolve, reject) => {
+        fetch(baseURL + '/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username: username, password: password}),
+        }).then((response) => {
+            if (response.ok) {
+                response.json().then((user) => {
+                    resolve(user);
+                });
+            } else {
+                // analyze the cause of error
+                response.json()
+                    .then((obj) => { reject(obj); }) // error msg in the response body
+                    .catch((err) => { reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
+            }
+        }).catch((err) => { reject({ errors: [{ param: "Server", msg: "Cannot communicate" }] }) }); // connection errors
+    });
+}
+
+async function userLogout(username, password) {
+    return new Promise((resolve, reject) => {
+        fetch(baseURL + '/logout', {
+            method: 'POST',
+        }).then((response) => {
+            if (response.ok) {
+                resolve(null);
+            } else {
+                // analyze the cause of error
+                response.json()
+                    .then((obj) => { reject(obj); }) // error msg in the response body
+                    .catch((err) => { reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
+            }
+        });
+    });
+}
+
+async function isAuthenticated(){
+    let url = "/user";
+    const response = await fetch(baseURL + url);
+    const userJson = await response.json();
+    if(response.ok){
+        return userJson;
+    } else {
+        let err = {status: response.status, errObj:userJson};
+        throw err;  // An object with the error coming from the server
+    }
+}
+
+const API = {deleteTask, addTask, getTasks, updateTask, isAuthenticated, userLogout, userLogin};
 export default API;
 
 /*export async function getPublicTasks() {
--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect } from 'react-router-dom';
 import API from './Api/API';
 import NavBarFilters from './Components/NavBarFilters';
 import NavBarMobile from './Components/NavBarMobile';
 import TasksList from './Components/Task';
 import ModalTask from './Components/ModalTask';
 import { useEffect } from 'react';
+import { LoginForm, LogoutButton } from './Components/LoginComponents';
 
 const filters = [
   { label: 'All', icon: 'inbox' },
@@ -26,44 +27,53 @@ function App() {
   const [update, setUpdate] = useState(true);
   const [showFil, setShowFil] = useState('');
   const [authUser, setAuthUser] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
 
   useEffect(() => {
-    if (update) {
+    const checkAuth = async => {
+      API.isAuthenticated().then(
+        (user) => {
+          setAuthUser(user);
+          setLoggedIn(true);
+        }
+      ).catch((err) => {
+        console.log("nessun utente loggato");
+      });
+    }
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    if (update && loggedIn) {
       //setTimeout(() => {
-        API.getTasks(showFil).then((t) => {
-          setTasks(t);
-          setUpdate(false);
-        });
+      API.getTasks(showFil).then((t) => {
+        setTasks(t);
+        setUpdate(false);
+      });
       //}, 1000);
     }
-  }, [update]);
-
+  }, [update, loggedIn]);
 
   //Add a logout method
-  logout = () => {
+  const logout = () => {
     API.userLogout().then(() => {
       setAuthUser(null);
+      setLoggedIn(false);
       setTasks([]);
     });
   }
 
   // Add a login method
-  login = (username, password) => {
-    API.userLogin(username, password).then(
-      (user) => { 
-        API.getTasks()
-          .then((tasks) => {
-            this.setState({tasks: tasks, projects: this.getProjects(tasks), authUser: user, authErr: null});
-            this.props.history.push("/tasks");
-          })
-          .catch((errorObj) => {
-            this.handleErrors(errorObj);
-        });
+  const login = (credentials) => {
+    API.userLogin(credentials.username, credentials.password).then(
+      (user) => {
+        setAuthUser(user);
       }
     ).catch(
       (errorObj) => {
         const err0 = errorObj.errors[0];
-        this.setState({authErr: err0});
+        this.setState({ authErr: err0 });
       }
     );
   }
@@ -75,7 +85,7 @@ function App() {
   const handleTaskList = {
     addTask: (task) => {
       setTasks(oldTasks => [{ id: oldTasks.length + 1, completed: task.completed, description: task.description, important: task.important, private: task.private, deadline: task.deadline }, ...oldTasks]);
-      API.addTask(task).then(()=>setUpdate(true));
+      API.addTask(task).then(() => setUpdate(true));
     },
 
     setEditTask: (task) => {
@@ -84,12 +94,12 @@ function App() {
 
     editTask: (task) => {
       setTasks(oldTasks => oldTasks.map((t) => { return t.id === task.id ? task : t }));
-      API.updateTask(task).then(()=>setUpdate(true));
+      API.updateTask(task).then(() => setUpdate(true));
     },
 
     deleteTask: (id) => {
       setTasks(oldTask => oldTask.filter(t => t.id !== id));
-      API.deleteTask(id).then(()=>setUpdate(true));
+      API.deleteTask(id).then(() => setUpdate(true));
     }
   }
 
@@ -99,7 +109,7 @@ function App() {
       if (f.label === filter) icon = f.icon;
       document.getElementById(`filter-${f.label}-icon`).classList.replace(`bi-${f.icon}-fill`, `bi-${f.icon}`);
       document.getElementById(`filter-mobile-${f.label}-icon`).classList.replace(`bi-${f.icon}-fill`, `bi-${f.icon}`);
-      setShowFil(filter);setUpdate(true);
+      setShowFil(filter); setUpdate(true);
     });
     if (filters.map(f => f.label).includes(filter)) {
       setSearch('');
@@ -112,21 +122,24 @@ function App() {
 
   return (
     <Router>
-        <Container fluid={true} className='pe-3 m-0'>
-          <Col className='p-0 m-0'>
-            <Row className='d-block d-lg-none bg-primary mb-5'><NavBarMobile open={open} setOpen={setOpen} filters={filters} setFilter={selectFilter} setSearch={setSearch} /></Row>
-            <Row>
-              <NavBarFilters filters={filters} setFilter={selectFilter}/>
-              <Col className='p-5 m-0 mr-md-4'>
-                <TasksList update={update} tasks={tasks} filters={filters} handleTaskList={handleTaskList} search={search} />
-              </Col>
-            </Row>
-          </Col>
-          <Button className='btn btn-lg btn-primary position-fixed rounded-circle' style={{ width: '3.5rem', height: '3.5rem', bottom: '2rem', right: '2rem', zIndex: '2' }} onClick={() => handleModalTask(true, undefined)}>
-            <i className='bi bi-plus-circle-dotted text-light d-flex justify-content-center' style={{ fontSize: '2rem' }} />
-          </Button>
-          {(modalTask.show) ? <ModalTask show={modalTask.show} task={modalTask.task} handleModalTask={handleModalTask} handleTaskList={handleTaskList} /> : <></>}
-        </Container>
+      <Container fluid={true} className='pe-3 m-0'>
+        <Row>
+          {loggedIn ? <LogoutButton logout={logout} /> : <Redirect to="/login" />}
+        </Row>
+        <Col className='p-0 m-0'>
+          <Row className='d-block d-lg-none bg-primary mb-5'><NavBarMobile open={open} setOpen={setOpen} filters={filters} setFilter={selectFilter} setSearch={setSearch} /></Row>
+          <Row>
+            <NavBarFilters filters={filters} setFilter={selectFilter} />
+            <Col className='p-5 m-0 mr-md-4'>
+              <TasksList update={update} tasks={tasks} filters={filters} handleTaskList={handleTaskList} search={search} loggedIn={loggedIn} login={login} />
+            </Col>
+          </Row>
+        </Col>
+        <Button className='btn btn-lg btn-primary position-fixed rounded-circle' style={{ width: '3.5rem', height: '3.5rem', bottom: '2rem', right: '2rem', zIndex: '2' }} onClick={() => handleModalTask(true, undefined)}>
+          <i className='bi bi-plus-circle-dotted text-light d-flex justify-content-center' style={{ fontSize: '2rem' }} />
+        </Button>
+        {(modalTask.show) ? <ModalTask show={modalTask.show} task={modalTask.task} handleModalTask={handleModalTask} handleTaskList={handleTaskList} /> : <></>}
+      </Container>
     </Router>
   );
 }
