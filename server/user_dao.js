e2662408637621d6e0716c1851f85408dff1e385
ANTONIO VESPA s284101 <44071720+mrwasp98@users.noreply.github.com>
2021-06-01 10:44:01 +0200

    update

---
+++ server/user_dao.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const db = require('./db');
+const bcrypt = require('bcrypt');
+
+/**
+ * Function to create a User object from a row of the users table
+ * @param {*} row a row of the users table
+ */
+
+function User(name, email, hash){  
+    this.name = name;
+    this.email = email;
+    this.hash = hash;
+}
+
+const createUser = function (row) {
+    const name = row.name;
+    const email = row.email;
+    const hash = row.hash;
+    return new User(name, email, hash);
+}
+
+exports.getUser = function (email) {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM users WHERE email = ?"
+        db.all(sql, [email], (err, rows) => {
+            if (err) 
+                reject(err);
+            else if (rows.length === 0)
+                resolve(undefined);
+            else{
+                const user = createUser(rows[0]);
+                bcrypt.compare(password, user.hash).then(result => {
+                    if(result)
+                        resolve(user);
+                    else
+                        resolve(false);
+                })
+                resolve(user);
+            }
+        });
+    });
+  };
