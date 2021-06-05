b50644445e0ed97bed08e85caa4db7eff957c7e1
ninosanta <38584348+ninosanta@users.noreply.github.com>
2021-06-05 20:05:44 +0200

    removed console prints for debugging

--- client/src/Api/API.js
+++ client/src/Api/API.js
@@ -1,5 +1,3 @@
-import dayjs from 'react-dayjs'
-
 const baseURL = "/api";
 
 async function getTasks(filter) {
@@ -88,13 +86,11 @@ async function userLogin(username, password) {
             body: JSON.stringify({username: username, password: password}),
         }).then((response) => {
             if (response.ok) {
-                console.log("yesss, loggato");
                 response.json().then((user) => {
                     resolve(user);
                 });
             } else {
                 // analyze the cause of error
-                console.log("login fallito");
                 response.json()
                     .then((obj) => { reject(obj); }) // error msg in the response body
                     .catch((err) => { reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
@@ -143,122 +139,3 @@ async function getUserInfo() {
 
 const API = {deleteTask, addTask, getTasks, updateTask, isAuthenticated, getUserInfo, userLogout, userLogin};
 export default API;
-
-/*export async function getPublicTasks() {
-    let url = "/tasks/public";
-
-    const response = await fetch(baseURL + url);
-    const tasksJson = await response.json();
-    if(response.ok){
-        //return tasksJson.map((t) => Task.from(t));
-        return tasksJson.map((t) => new Task(t.id,t.description,t.important, t.privateTask,t.deadline,t.project, t.completed, t.user));
-    } else {
-        let err = {status: response.status, errObj:tasksJson};
-        throw err;  // An object with the error coming from the server
-    }
-}
-
-export async function addTask(task) {
-    return new Promise((resolve, reject) => {
-        fetch(baseURL + "/tasks", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(task),
-        }).then( (response) => {
-            if(response.ok) {
-                resolve(null);
-            } else {
-                // analyze the cause of error
-                response.json()
-                .then( (obj) => {reject(obj);} ) // error msg in the response body
-                .catch( (err) => {reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
-            }
-        }).catch( (err) => {reject({ errors: [{ param: "Server", msg: "Cannot communicate" }] }) }); // connection errors
-    });
-}
-
-export async function updateTask(task) {
-    return new Promise((resolve, reject) => {
-        fetch(baseURL + "/tasks/" + task.id, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(task),
-        }).then( (response) => {
-            if(response.ok) {
-
-            } else {
-                // analyze the cause of error
-                response.json()
-                .then( (obj) => {reject(obj);} ) // error msg in the response body
-                .catch( (err) => {reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
-            }
-        }).catch( (err) => {reject({ errors: [{ param: "Server", msg: "Cannot communicate" }] }) }); // connection errors
-    });
-}
-
-
-export async function deleteTask(taskId) {
-    return new Promise((resolve, reject) => {
-        fetch(baseURL + "/tasks/" + taskId, {
-            method: 'DELETE'
-        }).then( (response) => {
-            if(response.ok) {
-                resolve(null);
-            } else {
-                // analyze the cause of error
-                response.json()
-                .then( (obj) => {reject(obj);} ) // error msg in the response body
-                .catch( (err) => {reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
-            }
-        }).catch( (err) => {reject({ errors: [{ param: "Server", msg: "Cannot communicate" }] }) }); // connection errors
-    });
-}
-
-async function userLogin(username, password) {
-    return new Promise((resolve, reject) => {
-        fetch(baseURL + '/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({username: username, password: password}),
-        }).then((response) => {
-            if (response.ok) {
-                response.json().then((user) => {
-                    resolve(user);
-                });
-            } else {
-                // analyze the cause of error
-                response.json()
-                    .then((obj) => { reject(obj); }) // error msg in the response body
-                    .catch((err) => { reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
-            }
-        }).catch((err) => { reject({ errors: [{ param: "Server", msg: "Cannot communicate" }] }) }); // connection errors
-    });
-}
-
-async function userLogout(username, password) {
-    return new Promise((resolve, reject) => {
-        fetch(baseURL + '/logout', {
-            method: 'POST',
-        }).then((response) => {
-            if (response.ok) {
-                resolve(null);
-            } else {
-                // analyze the cause of error
-                response.json()
-                    .then((obj) => { reject(obj); }) // error msg in the response body
-                    .catch((err) => { reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
-            }
-        });
-    });
-}
-
-const API = { isAuthenticated, getTasks, getPublicTasks, addTask, updateTask,deleteTask, userLogin, userLogout} ;
-
-*/
-
